25d3f039261cbee0045bdbd2e2d3f68e208ae91b
corinnegottleb <corinneottlieb@gmail.com>
2019-01-27 13:39:32 +0200

    first version complete

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking = $("#park-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
     renderApts(relevantApts)
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
-}
+    console.log(apartments) 
+    var source = $(`#apartment-template`).html();
+    var template = Handlebars.compile(source)
+    var newHTML = template({apartments})
+    $(`#results`).append(newHTML)}
+   
+
 
 renderApts(apartments) //renders apartments when page loads
